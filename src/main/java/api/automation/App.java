06e327d83abed0ec83f54ab65eee7356234abdfc
naren.gundeboina <naren.gundeboina@working-age.digital.dwp.gov.uk>
2021-01-21 23:16:13 +0000

    first-commit

---
+++ src/main/java/api/automation/App.java
@@ -0,0 +1,13 @@
+package api.automation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
